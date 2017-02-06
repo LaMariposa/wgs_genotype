efbaa9c41eefe6295ff0b75259f7415cf9ad3506
LaMariposa <msupple@ucsc.edu>
2017-02-06 12:22:23 -0800

    working

--- align_wgs.sh
+++ align_wgs.sh
@@ -5,7 +5,7 @@
 #M. Supple
 #last modified 3 February 2016
 
-#usage align_reads.sh </path/in/dir>
+#usage align_reads.sh </path/genome/genome.fasta> </path/in/dir>
 
 #requires
 	#bwa
@@ -16,7 +16,8 @@
 #output: bam file and index for each library
 
 #get input information
-indir=$1
+genome=$1
+indir=$2
 
 #get file list
 file1=($(ls $indir/*_R1_clean.fastq.gz))
@@ -27,7 +28,7 @@ file3=($(ls $indir/*_S_clean.fastq.gz))
 #check if have equal numbers of read1 and read2 and read3
 if [ ${#file1[@]} = ${#file2[@]} ] && [ ${#file2[@]} = ${#file3[@]} ] 
 	then
-		echo "processing ${#file1[@]} file triples"
+		echo -e "processing ${#file1[@]} file triples\naligning to genome $genome"
 else 
 	echo "Failed!  Number of R1, R2, S files differ"; exit 1
 fi
@@ -48,13 +49,27 @@ for ((a=0; a<${#file1[@]}; a++))
 		#determine output prefix
 		tempid=`basename ${file1[a]}`
 		fileid=${tempid%_R1*}
-		echo "$fileid"
+		echo "read group=$fileid"
+		#determine library id
+		libid=${fileid%_S*}
+		echo "library ID=$libid"
 
 
 		#align with bwa mem
 		#pairs
-		bwa mem -t 6 -PM -R '@RG\tID:$fileid\tSM:test'
+#		bwa mem -t 6 -PM -R "@RG\tID:$fileid\tLB:$libid\tSM:$libid\tPL:ILLUMINA" $genome \
+#			${file1[a]} ${file2[a]} > $outdir/temp_p.sam
 		#orphans
+#		bwa mem -t 6 -M -R "@RG\tID:$fileid\tLB:$libid\tSM:$libid\tPL:ILLUMINA" $genome \
+ #                       ${file3[a]} > $outdir/temp_u.sam
+		#generate bams and merge pairs and orphans and sort
+#		samtools view -b -o $outdir/temp_p.bam $outdir/temp_p.sam
+#		samtools view -b -o $outdir/temp_u.bam $outdir/temp_u.sam
+#		samtools merge -c $outdir/temp.bam $outdir/temp_p.bam $outdir/temp_u.bam
+#		samtools sort -o $outdir/temp_sort.bam -T tmpsort $outdir/temp.bam
+		#clean
+		rm $outdir/temp_p.sam $outdir/temp_u.sam $outdir/temp_p.bam $outdir/temp_u.bam $outdir/temp.bam
+
 
 		#mark duplicates with picard
 	
