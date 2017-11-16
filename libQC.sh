229f79069c9ead7b4a4f1f227c77ab630816d61a
LaMariposa <msupple@ucsc.edu>
2017-11-15 17:21:43 -0800

    added cleaning stats

--- libQC.sh
+++ libQC.sh
@@ -8,20 +8,25 @@
 #usage align_reads.sh </path/genome/genome.fasta> </path/in/dir>
 
 #requires
-	#fastqc
-	#bwa
-	#samtools 
-	#prinseq
+	#FastQC
+	#BWA
+	#SAMtools 
+	#PRINSEQ
+	#Trimmomatic
+	#FLASh
 
 #input: paired fastq files (assumes *_R1_001.fastq.gz and *_R2_001.fastq.gz)
 
 #output:
-	#fastqc report 
-	#sam file and fragment length distribution
-	#prinseq output
-	#flagstat
+	#fastqc reports 
+	#fragment length distribution from alignment
+	#flagstat output
+	#prinseq reorts
+	#stats from adapter trimming and merging
 
 threads=6
+adapters=/soe/megan/bin/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa
+trimmo=/soe/megan/bin/trimmomatic.jar
 
 #get input information
 genome=$1
@@ -47,11 +52,6 @@ fi
 fqcdir=fastqc
 if [ ! -d "$fqcdir" ]; then
   mkdir $fqcdir
-fi
-
-aligndir=align_bam
-if [ ! -d "$aligndir" ]; then
-  mkdir $aligndir
 fi
 
 tlendir=tlens
@@ -84,23 +84,47 @@ for ((a=0; a<${#file1[@]}; a++))
 		echo "read group=$fileid"
 
 		#align with bwa mem
-		bwa mem -t $threads -PM $genome ${file1[a]} ${file2[a]} > $aligndir/$fileid.sam
+		bwa mem -t $threads -PM $genome ${file1[a]} ${file2[a]} > temp.sam
 		echo -e "\n\n\nstats for $fileid" >> flagstat.out
-		samtools flagstat $aligndir/$fileid.sam >> flagstat.out
+		samtools flagstat temp.sam >> flagstat.out
 		
-		#get frag length distribution
-		cut -f9 -d$'\t' $aligndir/$fileid.sam > $tlendir/$fileid.tlen.txt
+		#get fragment length distribution
+		cut -f9 -d$'\t' temp.sam > $tlendir/$fileid.tlen.txt
+		rm temp.sam
 
 		#prinseq
-		zcat ${file1[a]} | perl /projects/redser3-notbackedup/projects/pheintzman/Scripts/prinseq-lite.pl -fastq stdin -graph_data $psdir/$fileid.R1.gd \
+		zcat ${file1[a]} | prinseq-lite.pl -fastq stdin -graph_data temp.R1.gd \
 			-graph_stats ld,gc,qd,ns,pt,ts,aq,de,da,sc -out_good null -out_bad null
-		zcat ${file2[a]} | perl /projects/redser3-notbackedup/projects/pheintzman/Scripts/prinseq-lite.pl -fastq stdin -graph_data $psdir/$fileid.R2.gd \
-                        -graph_stats ld,gc,qd,ns,pt,ts,aq,de,da,sc -out_good null -out_bad null
-		perl /projects/redser3-notbackedup/projects/pheintzman/Scripts/prinseq-graphs.pl -i $psdir/$fileid.R1.gd -o $psdir/$fileid.R1.graph_data -png_all -html_all 
-		perl /projects/redser3-notbackedup/projects/pheintzman/Scripts/prinseq-graphs.pl -i $psdir/$fileid.R2.gd -o $psdir/$fileid.R2.graph_data -png_all -html_all
+		zcat ${file2[a]} | prinseq-lite.pl -fastq stdin -graph_data temp.R2.gd \
+                       -graph_stats ld,gc,qd,ns,pt,ts,aq,de,da,sc -out_good null -out_bad null
+		prinseq-graphs.pl -i temp.R1.gd -o $psdir/$fileid.R1.graph_data -html_all 
+		prinseq-graphs.pl -i temp.R2.gd -o $psdir/$fileid.R2.graph_data -html_all
+
+		#adapter trim with trimmomatic
+		java -jar $trimmo PE -threads 4 ${file1[a]} ${file2[a]} -baseout temp \
+			ILLUMINACLIP:$adapters:2:30:10:1
+
+		#merge reads with flash
+		flash -M 140 -t 4 -z temp_1P temp_2P
+
+		#cleanup
+		rm temp.R1.gd temp.R2.gd
+		rm temp_1P temp_2P temp_1U temp_2U
+		rm out.notCombined_1.fastq.gz out.notCombined_2.fastq.gz out.extendedFrags.fastq.gz 
+		rm out.hist out.histogram
+		
 	done
 
+#make fragment length distributino
 Rscript /soe/megan/wgs_genotype/tlendist.r
+#get Trimmomatic stats
+grep 'read group=\|Input Read Pairs:' qc.out > cleaning.stats
+#get FLASh stats
+grep 'read group=\|Percent combined:' qc.out >> cleaning.stats
+
+#more cleaning
+rm -I fastqc/*zip
+rmdir temp
 
 echo -e "\n\nDONE!!!"
 
